c8efd2404da348d98c9839c7ce0f85bbd7281463
lkassianik <lyubakassianik@gmail.com>
2015-10-07 20:37:00 -0700

    updated blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008020104) do
+ActiveRecord::Schema.define(version: 20151008031506) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
+
+  add_index "posts", ["slug"], name: "index_posts_on_slug"
 
 end
